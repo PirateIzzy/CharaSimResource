0e17847ff935c62b122c1ccba5be6d7ce860c2ec
seotbeo <yoonv72@gmail.com>
2025-06-18 16:12:45 +0900

    2

--- Resource.Designer.cs
+++ Resource.Designer.cs
@@ -5203,6 +5203,16 @@ namespace CharaSimResource {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        public static System.Drawing.Bitmap UIToolTipNew_img_Item_Common_ItemIcon_base_custom {
+            get {
+                object obj = ResourceManager.GetObject("UIToolTipNew_img_Item_Common_ItemIcon_base_custom", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         public static System.Drawing.Bitmap UIToolTipNew_img_Item_Common_ItemIcon_shade {
             get {
                 object obj = ResourceManager.GetObject("UIToolTipNew_img_Item_Common_ItemIcon_shade", resourceCulture);
